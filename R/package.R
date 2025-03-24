6aaefdc58c1b221fec36bd739a834f08d0c863a9
rCarto <timothee.giraud@cnrs.fr>
2025-03-24 09:56:05 +0100

    initial commit

---
+++ R/package.R
@@ -0,0 +1,20 @@
+#' @title Shortest Paths and Travel Time from OpenStreetMap via an Valhalla API
+#' @name val
+#' @description An interface between R and the Valhalla API.\cr
+#' Valhalla is a routing
+#' service based on OpenStreetMap data.
+#' See <https://valhalla.github.io/valhalla/> for more
+#' information. This package enables the computation of routes, trips,
+#' isochrones and
+#' travel distances matrices (travel time and kilometric distance).\cr
+#'
+#'
+#' @note
+#' This package relies on the usage of a running Valhalla service (tested with
+#' version xxx API).\cr
+#'
+#' The package ships a sample dataset of 100 random pharmacies in Berlin
+#' (© OpenStreetMap contributors - \url{https://www.openstreetmap.org/copyright/en}).\cr
+#' The sf dataset uses the projection WGS 84 / UTM zone 34N (EPSG:32634).\cr
+#' The csv dataset uses WGS 84 (EPSG:4326).\cr
+"_PACKAGE"
