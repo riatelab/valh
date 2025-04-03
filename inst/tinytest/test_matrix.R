5fc053f713b2cb82a36709a0d73ab800342dc59d
rCarto <timothee.giraud@cnrs.fr>
2025-04-03 16:28:24 +0200

    test: add tests for vl_route()

--- inst/tinytest/test_matrix.R
+++ inst/tinytest/test_matrix.R
@@ -1,15 +1,15 @@
 if(demo_server){
-  A <- vl_matrix(src = x_df[1:10, ], dst = x_df[11:20, ])
-  expect_true(length(A)==4)
-  expect_equal(dim(A$sources), c(10,2))
-  expect_equal(dim(A$destinations), c(10,2))
-  expect_equal(dim(A$durations), c(10,10))
-  expect_equal(dim(A$distances), c(10,10))
+  x <- vl_matrix(src = x_df[1:10, ], dst = x_df[11:20, ])
+  expect_true(length(x)==4)
+  expect_equal(dim(x$sources), c(10,2))
+  expect_equal(dim(x$destinations), c(10,2))
+  expect_equal(dim(x$durations), c(10,10))
+  expect_equal(dim(x$distances), c(10,10))
   wait()
-  expect_silent(B <- vl_matrix(loc = x_sf[1:10,],
+  expect_silent(y <- vl_matrix(loc = x_sf[1:10,],
                                costing = "bicycle",
                                costing_options = list(bicycle_type = "Road")))
   wait()
-  expect_true(max(diag(B$durations)) == 0)
-  expect_identical(row.names(B$durations), row.names(x_sf[1:10, ]))
+  expect_true(max(diag(y$durations)) == 0)
+  expect_identical(row.names(y$durations), row.names(x_sf[1:10, ]))
 }
