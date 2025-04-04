a37aa449b1288dc54dbc3b9ca3570a6e6a71f5b5
rCarto <timothee.giraud@cnrs.fr>
2025-04-04 10:38:28 +0200

    test: fix test

--- inst/tinytest/test_status.R
+++ inst/tinytest/test_status.R
@@ -5,5 +5,5 @@ if(demo_server){
   expect_equal(length(status), 3)
   expect_equal(names(status), c("version", "tileset_last_modified", "available_actions"))
   status_v <- vl_status(verbose = TRUE)
-  expect_true(length(status_v) > 3)
+  expect_true(length(status_v) >= 3)
 }
